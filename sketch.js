258f381ae26449b703ccb662ccf24edf8f242c0b
vibhor1107 <65174758+vibhor1107@users.noreply.github.com>
2020-05-21 16:42:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var bullet,wall;
+var speed ,weight,thickness ;
+
+
+
+function setup() {
+  speed=random(223,321);
+  weight=random(30,52);
+  thickness=random(22,83);
+  createCanvas(1600,400);
+  
+  bullet = createSprite(50,200,100,75);
+  bullet.velocityX = speed;
+  wall=createSprite(1200,200,thickness,height/2);
+  wall.shapeColor=color(80,80,80);
+}
+
+function draw() {
+  background(255,255,255);  
+ if (bullet.isTouching(wall)){
+   bullet.velocityX=0;
+   var damage= 0.5*weight*speed*speed/(thickness*thickness*thickness);
+   if (damage>10){
+     wall.shapeColor=color(255,0,0);
+   }
+   if (damage<10){
+     wall.shapeColor=color(0,255,0);
+   }
+ }
+  
+  
+  drawSprites();
+}
+
+
+
